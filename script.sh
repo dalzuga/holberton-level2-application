afc8f3e6762669dcd1e57143eb57c4dc040e5048
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-22 13:26:30 -0800

    asdf

--- script.sh
+++ script.sh
@@ -9,3 +9,7 @@ git config --global user.name "Daniel Alzugaray"
 git config --global user.email "dalzuga@gmail.com"
 git config --global core.editor emacs
 git config --global push.default simple
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
