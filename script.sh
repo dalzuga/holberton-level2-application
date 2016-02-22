3e7e575ce817ba9fcb453d818ff2006b70f396d7
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-22 13:23:12 -0800

    asdf

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PS1="\[\e[0;36m\]\h\[\e[m\]:\W \[\e[34m\]\u\[\e[m\]\[\e[31m\]\$\[\e[m\] "
+#ssh-keygen -t rsa -b 4096 -C "dalzuga@gmail.com"
+#cat ~/.ssh/id_rsa.pub
+#open https://github.com/settings/ssh
 
-ssh-keygen -t rsa -b 4096 -C "dalzuga@gmail.com"
-cat ~/.ssh/id_rsa.pub
-open https://github.com/settings/ssh
+PS1="\[\e[0;36m\]\h\[\e[m\]:\W \[\e[34m\]\u\[\e[m\]\[\e[31m\]\$\[\e[m\] "
 
 git config --global user.name "Daniel Alzugaray"
 git config --global user.email "dalzuga@gmail.com"
