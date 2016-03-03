961ca2f87b6b1e6894374848737ca33dbe1b8e72
Daniel Alzugaray <dalzuga@users.noreply.github.com>
2016-03-03 09:04:25 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,4 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
